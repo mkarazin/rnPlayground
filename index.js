152f518161bf9cfba9ecdec3b2d7d0a43c3715e2
Matt Karazin <karazin@gmail.com>
2020-03-19 15:40:46 -0600

    Fixed some styling shit

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 // this NEEDS to be the first thing in our app
 import 'react-native-gesture-handler';
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
