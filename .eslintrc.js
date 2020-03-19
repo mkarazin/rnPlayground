5bec979b8e6d8e2ab255aa06e71626a2c1323f3e
Matt Karazin <karazin@gmail.com>
2020-03-19 12:27:17 -0600

    added additional react and react-native specific eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,26 @@ module.exports = {
     'plugin:flowtype/recommended'
   ],
   parser: 'babel-eslint',
-  plugins: ['flowtype'],
-  rules: {}
+  parserOptions: {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  env: {
+    "react-native/react-native": true,
+  },
+  plugins: [
+    'flowtype',
+    'react',
+    'react-native'
+  ],
+  rules: {
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+  }
 };
