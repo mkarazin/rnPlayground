cf13f2643f7467d139dae629d29d5a875819b374
Matt Karazin <karazin@gmail.com>
2020-03-19 15:34:35 -0600

    react-navigation installation

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /**
  * @format
  */
+// According to react navigation installation instructions (and gesture-handler's)
+// this NEEDS to be the first thing in our app
+import 'react-native-gesture-handler';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
